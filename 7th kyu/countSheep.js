e79e3f516353c03bb9b071cf451c5fb241520969
ann5306 <annkiunga@gmail.com>
2024-05-09 09:24:33 +0300

    Edit countSheep.js

--- 7th kyu/countSheep.js
+++ 7th kyu/countSheep.js
@@ -1,3 +1,7 @@
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers. in javascript please.
 function countSheep (num) {
     // Initialize an empty string to store the murmur
     let murmur = "";
