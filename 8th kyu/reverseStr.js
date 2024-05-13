90e2681ec5b096324312de4bf6a91e63c94a5366
ann5306 <annkiunga@gmail.com>
2024-05-13 12:30:21 +0300

    Add reverseStr.js solution.

---
+++ 8th kyu/reverseStr.js
@@ -0,0 +1,8 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function reverseStr(str){
+    return str.split("").reverse().join("")
+  }
